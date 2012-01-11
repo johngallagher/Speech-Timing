7caf41965fd56003d737188c5c3fd6de4043393d
John Gallagher <john@jgediting.co.uk>
2012-01-11 17:32:19 +0000

    Added todo

--- Classes/JGAlert.h
+++ Classes/JGAlert.h
@@ -8,6 +8,6 @@
 @property(nonatomic, retain) NSDate *startTime;
 @property(nonatomic, retain) NSDate *fireTime;
 
-
+// TODO make factory method alertStartingNowWithDuration
 -(NSTimeInterval)duration;
 @end
